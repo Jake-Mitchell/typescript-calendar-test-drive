1144e82554ae27041cec1724cf8544e092c56f9d
Jake-Mitchell <39348241+Jake-Mitchell@users.noreply.github.com>
2022-10-03 10:22:40 -0700

    shows a calendar of mondays for the month of April

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,13 +1,50 @@
 import { Date, Weekday } from "../../../types";
 import { Weekdays } from "../../configs/Weekdays";
+import { monthDates } from "../../configs/Monthdays";
 import s from "./Calendar.module.css";
+import { ChevronLeftIcon, ChevronRightIcon } from "../../assets/icons";
+
+const generateDates = (date: number) => {
+  for (let i = 0; i < 7; i++) {
+    return (
+      <button className={s.date} value={date}>
+        <p className={`${s.day} ${date === 18 ? s.today : ""}`}>{date}</p>
+      </button>
+    );
+  }
+};
+
+const generateWeeks = (dates: Array<Date>) => {
+  const daysInWeek = 7;
+  let result = []; // there may be a better name for this!
+
+  for (let i = 0; i < dates.length; i += daysInWeek) {
+    result.push(dates.slice(i, i + daysInWeek));
+  }
+
+  return result;
+};
 
 export const Calendar: React.FC<{}> = ({}) => {
   return (
     <div className={s.container}>
-      <div className={s.datePickerContainer}></div>
-      <div className={s.weekdaysContainer}></div>
-      <div className={s.calendar}></div>
+      <div className={s.datePickerContainer}>
+        <ChevronLeftIcon />
+        <span>February 2023</span>
+        <ChevronRightIcon />
+      </div>
+      <div className={s.weekdaysContainer}>
+        {Weekdays.map((day) => (
+          <div className={s.weekday}>{day}</div>
+        ))}
+      </div>
+      <div className={s.calendar}>
+        {generateWeeks(monthDates).map((week) => (
+          <div className={s.week}>
+            {week.map((day) => generateDates(day.day))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Calendar } from "../calendar/Calendar";
 import s from "./DatePicker.module.css";
 
 export const DatePicker: React.FC<{}> = ({}) => {
   return (
-    <div className={s.datePickerContainer}>
+    <div className={s.container}>
       <div className={s.backgroundContainer} />
+      <div className={s.calendarContainer}>
+        <Calendar />
+      </div>
     </div>
   );
 };
